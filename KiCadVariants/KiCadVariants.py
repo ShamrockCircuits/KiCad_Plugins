eca1dcb24af67957dd02981d35062d7f5b45cd35
Jesse Farrell <jessefarrell92@gmail.com>
2024-11-23 11:59:12 -0800

    Add more verbose docstrings to methods

--- KiCadVariants/KiCadVariants.py
+++ KiCadVariants/KiCadVariants.py
@@ -19,11 +19,15 @@ class ChildVariant():
     default_value = CurrentDisplayed
 
     def __init__(self, variant_name : str,  Schematic_List : List['skip.Schematic']):
-        self.name =  variant_name.replace("Variant_", "")   # remove the Variant tag if it was already there
-        self.property_name = "Variant_" + variant_name
-        self.DNP = False
+        '''
+            @param - variant_name: name to be assigned to variant (ex: 'BASE")\n
+            @param - Schematic_List: List of all schematics in the project
+        '''
 
-        self.Sch_List = Schematic_List  # this is bad practice... probably
+        self.name =  variant_name.replace("Variant_", "")   # Remove the Variant tag if it was already there
+        self.property_name = "Variant_" + variant_name      
+        self.DNP = self.default_value                       # Overwritten when we call __AddSymbolProperties()
+        self.Sch_List = Schematic_List
 
         # Check that this variant is already listed on the schematic part properties
         self.__AddSymbolProperties()
@@ -55,7 +59,8 @@ class ChildVariant():
 
     def Remove_Variant(self):
         '''
-            Removes this variant from the schematic. 
+            Removes this variant from the schematic.\n
+            NOTE - Must call SAVE() for changes to stick.
         '''
 
         for Schematic in self.Sch_List:
@@ -69,12 +74,11 @@ class ChildVariant():
 
     def LoadDNPtoVariant(self):
         '''
-        This method copies the current population (DNP) displayed on the schematic and updates THIS variant with that population.
+        This method copies the current population (DNP) displayed on the schematic and updates THIS variant with that population.\n
+        Example - If your currently displaying the BASE variant, then call this method on the LITE variant, the BASE and LITE variant will \n
+        now have the identical populations.\n
 
-        Example - If your currently displaying the BASE variant, then call this method on the LITE variant, the BASE and LITE variant will 
-        now have the identical populations.
-
-        Assumes Variant-xxx property already exists (must have already called AddSymbolProperties)
+        Assumes Variant-xxx property already exists (must have already called AddSymbolProperties) \n
         '''
 
         Variant_property = None
@@ -94,6 +98,7 @@ class ChildVariant():
                     Variant_property.value = self.DoNotPopulate
                 else:
                     Variant_property.value = self.Populate
+        return None
 
     def DisplayThisVariant(self):
         '''
@@ -129,9 +134,24 @@ class Variants():
     # TODO - Add autosave feature w/ suggestions on when to reload kicad
     def __init__(self, Project_Schematics : list[str] = None):
         '''
-            @ param - Schematics. List of paths to schematics in the project.
-                    - If no paths are provided this constructor tries to find schematics based on working DIR
+            @ param - Schematics. List of paths to schematics in the project.\n
+                    - If no paths are provided this constructor tries to find schematics based on working DIR\n
+            TODO - I should probably just ask for the project directory and pull all <kicad_sch> files from there\n
+                 - instead of prompting the user for all the paths... 
         '''
+        print("========================================================\n"
+              "==================== KiCad Variants ====================\n"
+              "========================================================\n"
+              "  Backup your project before starting!!!  \n"
+              "========================= DEMO =========================\n"
+              " > paths = [<path-to-sch.kicad_sch>, <path-to-sch.kicad_sch>]\n"
+              " > my_Variants = KiCadVariants.Variants(paths)\n"
+              " > my_Variants.ListAllVariants()\n"
+              " > my_Variants.Add_Variant('TEST')\n"
+              " > my_Variants.Remove_Variant('BASE')\n"
+              " > my_Variants.SAVE()\n"
+              "=======================================================\n")
+
         self.Paths = Project_Schematics
         self.Sch_List : List['skip.Schematic'] = []
         self.Variant_List : list['ChildVariant'] = []
@@ -152,32 +172,37 @@ class Variants():
         return
 
     def SAVE(self):
+        '''
+            Saves data to all schematics. \n
+            Warning, this overwrites existing data. Best to keep a backup just to be safe.
+        '''
+
         for sch in self.Sch_List:
             sch.write(sch.filepath)
             pass
 
     def Reload_Schematic(self):
         '''
+            TLDR - Reloads sexpressions
+
             If you made changes to the schematic and want them to be reflected in your code, you will need to reload the schematic.
             For example, In Kicad eeschema you set the DNP's you'd like to load into the BASE variant, and save you changes.
             We must reload the sexpressions in order to "See" this change, before we can call "LoadDNPtoVariant".
 
-            TLDR - Reloads sexpressions
-
             BEWARE - Any changes you made will be lost unless you call "Variant.SAVE"
         '''
 
-        print("BEWARE - Any changes you made will be lost unless you call 'Variant.SAVE()'...\n" 
-              "Variant.SAVE() will also overwrite any changes on the schematic..\n"
-              "Recommend steps --- Call Variant.SAVE() -> Make DNP changes -> Call Reload_Schematic -> continue as normal")
+        # print("BEWARE - Any changes you made will be lost unless you call 'Variant.SAVE()'...\n" 
+        #       "Variant.SAVE() will also overwrite any changes on the schematic..\n"
+        #       "Recommend steps --- Call Variant.SAVE() -> Make DNP changes -> Call Reload_Schematic -> continue as normal")
 
         # I believe this will clear everything for us
         self.__init__(self.Paths) 
          
     def Add_Variant(self, variant_name : str):
         '''
-            Adds variant to this project.
-            @ param - variant_name. Name that will be assigned to the variant object
+            Adds variant to this project. If Variant already exists, nothing is done. \n
+            @ param - variant_name. Name that will be assigned to the variant object. EX: "BASE" will become "Variant_BASE"
         '''
 
         if( variant_name.__contains__("Variant")):
@@ -194,8 +219,8 @@ class Variants():
 
     def Remove_Variant(self, variant_name : str):
         '''
-            Tries to remove variant_name from existing variants.
-            Does not check if variant exists
+            Remove variant_name from existing variants.\n
+            Note - Call SAVE() to have these changes reflected on the schematic. \n
         '''
         var = self.__Get_Variant(variant_name)
         if type(var) != None:
@@ -204,20 +229,29 @@ class Variants():
         return
 
     def LoadDNPtoVariant(self, variant_name : str):
+        '''
+            Loads what's 'currently' displayed on the schematic into the varient you specify.\n
+            NOTE - You may need to call __Reload_Schematic() if you recently changed the schematic.\n
+        '''
+
         self.__Get_Variant(variant_name).LoadDNPtoVariant()
         return
 
     def Display_Variant(self, variant_name:str):
         '''
-        Updates the variant that's currently displayed on the schematic
-        Script parses every symbol and checks its "Variant-xxx" property.
-        If the name matches, then it updates the DNP attribute which is then displayed on the schematic
+        Updates the variant that's currently displayed on the schematics DNP.\n
+        Script parses every symbol and checks its "Variant-xxx" property. \n
+        If the name matches, then it updates the DNP attribute which is then displayed on the schematic. \n
+        NOTE - The user must call SAVE() then close and reopen the schematic editor to see the changes.
         '''
         self.__Get_Variant(variant_name).DisplayThisVariant()
 
         return
 
     def ListAllVariants(self):
+        '''
+            Prints all the available variants.\n
+        '''
 
         txt = "Available Variants -> ["
 
@@ -234,27 +268,33 @@ class Variants():
 
     def __Get_Variant(self, variant_name:str) -> ChildVariant:
         '''
-            Returns variant from Variant_List of the specified name
-            @param - variant_name. Name of desired variant
+            Returns variant from Variant_List of the specified name \n
+            Returns None if variant doesn't exists \n
+            @param - variant_name. Name of desired variant.
         '''
 
         for var in self.Variant_List:
             if var.name == variant_name:
-                print("__Get_Varaint() --> Found Variant")
+                # print("__Get_Varaint() --> Found Variant")
                 return var
         
-        print("__Get_Varaint() --> Not Found")
+        # print("__Get_Varaint() --> Not Found")
         return None
 
-    def __Load_Existing_Variants(self):
+    def __Load_Existing_Variants(self) -> bool:
         '''
-        NOTE - I don't like this method, nor how I implemented it.
-        Check what variants exist on the schematic by parsing all part properties
-        If symbols are missing variants, this property will be added.
+        Parses every schematic in the project and looks for unique Variants. \n
+        Loads Variants to module, as if user called "Add_Variant()" for each one \n
+        If symbols are missing variants, they will be added. \n
+
+        return - IssueFoundFlag - Generic flag notifying the user if any new variants had to be assigned\n
+        You can safely ignore the status of IssueFoundFlag. \n
+
+        TODO - Cleanup this method, its kinda greasy
         '''
 
         keyword = "Variant_"            # We will look for this keyword in the symbol properties
-        VaraintsFound = set()             # Reminder - Sets Only allows unique elements
+        VaraintsFound = set()           # Reminder - Sets Only allows unique elements
 
         IssueFoundFlag = False          # Very generic flag, just means the len(VariantsFound) wasn't the same for all the parts
 
@@ -282,10 +322,10 @@ class Variants():
                     # Find variant properties ex:"Variant_BASE"
                     if str(property.name).__contains__(keyword):
                         NumVariantsFound += 1
-
                         VaraintsFound.add(str(property.name).replace(keyword, ""))
 
                 # Determine if this part was (A) missing a varaint, or (B) had a variant that didn't exist before
+                # This is just to notify the user, nothing is really done with the information
                 if NumVariantsBefore != len(VaraintsFound):
                     print("__Load_Existing_Variants() - Found discrepancy in number of varaints... Seems like a new variant was added by " + str(part.name))
                     IssueFoundFlag = True
@@ -298,16 +338,15 @@ class Variants():
         for var in VaraintsFound:
             self.Add_Variant(var)
 
-        return
+        return IssueFoundFlag
 
     def __AutoPopulateSchPaths(self, search_path : str):
         '''
-            Finds all schematics in "Search_path" with ext (.kicad_sch)
-            Beware this method is somewhat recursive and will recall itself is some issue occurs
+            Finds all schematics in "Search_path" with ext (.kicad_sch) \n
+            Beware this method is somewhat recursive and will recall itself if some issue occurs\n
+            The schematics will be stored in self.Sch_List \n
             
-            @ Param - search_path. Existing DIR to search
-
-            Return - Updates self.Sch_List with new files
+            @param - search_path: Existing DIR to search\n
         '''
         file_ext = ".kicad_sch"
         path_list = []
@@ -334,8 +373,8 @@ class Variants():
        
     def __ReloadProject(self):
         '''
-            Saves then opens all .kicad_sch files.
-            TODO - Implement this method
+            Ideally this method would actually close and reopen the schematic for the user...\n
+            But for now it will just prompt the user to do it. \n
         '''
-        print("Please close and reopen the Kicad Schematics...")
+        input(">> Close and reopen schematic... *enter* to continue")
         return
